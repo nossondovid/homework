c00a5f080d62cd4ae69496d81a2692e6c9ed4df8
nossondovid <natepn@gmail.com>
2023-05-31 22:57:35 +0300

    c#quiz

---
+++ html/51/nosson_piskun_c#quiz.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "red", "green", "blue" };
+            string[] patterns = { "striped", "checked", "polka_dot" };
+            ArrayList shirtList = new ArrayList();
+
+            for (int i = 0; i < colors.Length; i++) 
+            {
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                    Shirt shirt = new Shirt();
+                    shirt.color = colors[i];
+                    shirt.pattern = patterns[j];
+                    shirtList.Add(shirt);
+                }
+            }
+
+            foreach (var shirt in shirtList)
+                Console.WriteLine(shirt.ToString());
+        }
+
+        class Shirt 
+        {
+            public string color;
+            public string pattern;
+
+            public override string ToString()
+            {
+                return color + ":" + pattern;
+            }
+        }
+
+
+    }
+}
